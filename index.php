9cee34cbddb9ee99d66791e2e696f9aa86f45fd3
brunohs007 <brunoholandasilva@hotmail.com>
2022-11-12 17:49:32 -0300

    estilizando a pagina

--- index.php
+++ index.php
@@ -1,44 +1,116 @@
 <html>
 	<head>
-		<title> MEU CEP </title>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Bootstrap CSS -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+		
+		<title> Busca CEP </title>
+
+		<style>
+		.card-login {
+			padding: 30px 0 0 0;
+			width: 500px;
+			margin: 0 auto;
+		}
+
+		.button {
+			margin-top: 10%;
+		}
+
+		.formulario {
+			text-align: center;
+		}
+		</style>
 	</head>
 	<body> 
-		<form action="index.php" method="post">
-			<label> Insira o CEP: </label>
-			<input type="text" name="cep">
-			<input type="submit" value="Enviar">
-		</form>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">Busca Cep</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+					<div class="navbar-nav">
+						<a class="nav-link active" aria-current="page" href="#">Pagina Principal</a>
+					</div>
+				</div>
+			</div>
+		</nav>
+		<div class="container">    
+			<div class="row">
+				<div class="card-login">
+					<div class="card">
+						<div class="card-header">
+							<h1> Bem vindo ao busca Cep! </h1>
+						</div>
+						<div class="card-body">
+							<form class="formulario" action="index.php" method="post">
+								<div class="form-group">
+									<label> Insira o CEP: </label>
+									<input class="form-control-md is-valid" type="text" name="cep" placeholder="Digite um cep!">
+								</div>
+								<button type="submit" value="Enviar" class="btn btn-primary  btn-info btn-block mb-3 button">Enviar</button>
+							</form>
+							<?php
+								//condicional que verifica se a variavel não é vazia
+								if(!empty($_POST['cep'])){
+									
+									//variavel cep recebe o valor do cep digitado
+									$cep = $_POST['cep'];
 
-		<?php
-			if(!empty($_POST['cep'])){
-				
-				$cep = $_POST['cep'];
+									//instanciando o objeto (atribuindo a classe Address a variavel address)
+									$address = new Address();
 
-				$address = new Address();
-				$address -> get_address($cep);
+									//executando o metodo get_address passando o cep digitado como parametro
+									$address -> get_address($cep);
 
-				$cep = preg_replace("/[^0-9]/", "", $cep);
-				echo "<br><br>CEP Informado: $cep<br>";
-				echo "Rua: $address->logradouro<br>";
-				echo "Bairro: $address->bairro<br>";
-				echo "Estado: $address->uf<br>";
-			}
+									//formatando o cep, substintuindo o caracter '-' por vazio
+									$cep = preg_replace("/[^0-9]/", "", $cep);
 
-			class Address {
-				public $logradouro;
-				public $bairro;
-				public $uf;
+									//exibindo as informações adquiridas a partir da chamada da funcao get_address
+									echo "<br><br>CEP Informado: $cep<br>";
+									echo "Rua: $address->logradouro<br>";
+									echo "Bairro: $address->bairro<br>";
+									echo "Estado: $address->uf<br>";
+								}else{
+									echo "Por favor, digite um cep!";
+								}
 
-				function get_address($cep){
-					$cep = preg_replace("/[^0-9]/", "", $cep);
-					$url = "http://viacep.com.br/ws/$cep/xml/";
-					$xml = simplexml_load_file($url);
-					$this->logradouro = $xml->logradouro;
-					$this->bairro = $xml->bairro;
-					$this->uf = $xml->uf;
-					return $xml;
-				}
-			}
-		?>
+								class Address {
+									//atributos
+									public $logradouro;
+									public $bairro;
+									public $uf;
+
+									//método recebendo o cep informado pelo usuario
+									function get_address($cep){
+
+										//formatando o cep, substintuindo o caracter '-' por vazio
+										$cep = preg_replace("/[^0-9]/", "", $cep);
+
+										//a partir desse ponto do codigo passa a ocorrer alguns warnings, caso o cep informado não exista, deveria ser implementado um tratamento para infromar o erro ao usuario
+									
+										//atribuindo um endereço eletronico na variavel url e passando o valor do cep
+										$url = "http://viacep.com.br/ws/$cep/xml/";
+
+										// carrega o conteudo do parametro url dentro de um objeto
+										$xml = simplexml_load_file($url);
+
+										//atribiu dados do xml recebido para os respectivos atributos
+										$this->logradouro = $xml->logradouro;
+										$this->bairro = $xml->bairro;
+										$this->uf = $xml->uf;
+										return $xml;
+									}
+								}
+							?>
+						</div>
+					</div>
+				</div>
+			</div>
+    	</div>
 	</body>
 </html>
